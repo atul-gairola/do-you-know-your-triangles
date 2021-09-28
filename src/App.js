fa82d670242f5940f4e02968dabbc6bd659269e9
atul-gairola <atulgairola08@gmail.com>
2021-09-29 01:56:05 +0530

    feat: create nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentNav, setCurrentNav] = useState(0);
+
+  const handleNavChange = (e) => {
+    const { id } = e.target;
+    setCurrentNav(Number(id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <div id={0} onClick={handleNavChange} className="logo">
+          Do you know your triangle ?
+        </div>
+        <ul className="linksContainer">
+          <li
+            id={1}
+            className={currentNav === 1 && "activeLink"}
+            onClick={handleNavChange}
+          >
+            Quiz
+          </li>
+          <li
+            id={2}
+            className={currentNav === 2 && "activeLink"}
+            onClick={handleNavChange}
+          >
+            Calculate Hypotenuse
+          </li>
+          <li
+            id={3}
+            className={currentNav === 3 && "activeLink"}
+            onClick={handleNavChange}
+          >
+            Calculate Area
+          </li>
+          <li
+            id={4}
+            className={currentNav === 4 && "activeLink"}
+            onClick={handleNavChange}
+          >
+            Check Angles
+          </li>
+        </ul>
+      </nav>
+      <p>{currentNav}</p>
+      <footer>
+        Create with love by <a href="https://atulgairola.tech">Atul Gairola</a>
+      </footer>
     </div>
   );
 }
